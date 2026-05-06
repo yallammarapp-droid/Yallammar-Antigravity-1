d3b0aab41c9c97260034b4394e3eca7301dcec17
yousef-taleb <yousefahmed.future2@gmail.com>
2026-05-06 08:40:52 +0300

    Update Website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,9 +46,7 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl" className={tajawal.variable}>
       <head>
-        <Script
-          id="google-tag-manager"
-          strategy="beforeInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
