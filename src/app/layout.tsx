190af19019a2acfd45ffed4faf9ffc1a3f27ea84
yousef-taleb <yousefahmed.future2@gmail.com>
2026-05-06 07:52:02 +0300

    Update Website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,19 @@ export default function RootLayout({
     <html lang="ar" dir="rtl" className={tajawal.variable}>
       <head>
         <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N47993TJ');
+            `
+          }}
+        />
+        <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=AW-18071421889`}
         />
@@ -86,6 +99,14 @@ export default function RootLayout({
         />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N47993TJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
       </body>
     </html>
